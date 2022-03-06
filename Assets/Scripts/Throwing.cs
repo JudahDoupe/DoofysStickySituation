2c733e2bd8237de8d1e4ec5324049c021c1141ed
JudahDoupe <jjdoupe@sep.com>
2022-03-06 11:31:36 -0500

    throwing

--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -1,36 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class Throwing : MonoBehaviour
 {
 
-    private List<GameObject> throwables = new List<GameObject>();
-    public GameObject throwLoc;
     public float upThrow = 10;
     public float fwdThrow = 10;
-    private GameObject grabbedObj;
     public GameObject lr;
     public GameObject RightHandTarget;
     public GameObject LeftHandTarget;
     [Range(0,1)]
     public float GrabTime;
 
+    public Vector2 BallOffset;
+
+    private List<GameObject> balls = new List<GameObject>();
+    private GameObject ball;
+
     private bool holding = false;
     private bool grabbing = false;
     private bool throwing = false;
+
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (throwables.Count > 0 && !holding)
+            if (balls.Count > 0 && !holding)
             {
-                grabbedObj = Nearest();
-                StartCoroutine(Grab(grabbedObj));
+                ball = Nearest();
+                StartCoroutine(Grab());
             }
-            else if (throwables.Count > 0 && holding && !grabbing)
+            else if (balls.Count > 0 && holding && !grabbing)
             {
                 holding = false;
                 Throw();
@@ -40,45 +45,68 @@ public class Throwing : MonoBehaviour
 
         if (holding && !grabbing && !throwing)
         {
-            RightHandTarget.transform.position = throwLoc.transform.position;
+            var center = GetTargetBallLocation();
+            var radius = GetBallRadius();
+            RightHandTarget.transform.position = center + transform.right * radius;
+            LeftHandTarget.transform.position = center - transform.right  * radius;
+            ball.transform.position = GetTargetBallLocation();
+            lr.transform.LookAt(lr.transform.position - transform.right);
+            lr.transform.position = ball.transform.position;
+        }
+        if (holding || grabbing  || throwing)
+        {
+            RightHandTarget.transform.rotation = 
+                Quaternion.LookRotation(transform.position - RightHandTarget.transform.position, Vector3.up);
         }
     }
 
-    IEnumerator Grab(GameObject ball)
+    IEnumerator Grab()
     {
         holding = true;
         grabbing = true;
 
         var time = 0f;
         var halfTime = GrabTime / 2f;
+        var radius = GetBallRadius();
 
-        var startPos = RightHandTarget.transform.position;
-        var endPos = ball.transform.position;
+        var startRotation = transform.rotation;
+        var leftStartPos = LeftHandTarget.transform.position;
+        var rightStartPos = RightHandTarget.transform.position;
+
+        var endRotation =
+            Quaternion.LookRotation(
+                Vector3.Scale(ball.transform.position - transform.position, new Vector3(1, 0, 1)).normalized,
+                Vector3.up);
+        var leftEndPos = ball.transform.position - (endRotation * Vector3.right) * radius;
+        var rightEndPos = ball.transform.position + (endRotation * Vector3.right) * radius;
 
         ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-        ball.transform.parent = RightHandTarget.transform;
-        ball.transform.localPosition = Vector3.zero;
 
         while (time < halfTime)
         {
             var t = time / halfTime;
-            RightHandTarget.transform.position = Vector3.Lerp(startPos, endPos, t);
-            ball.transform.localPosition = Vector3.Lerp(ball.transform.localPosition, Vector3.zero, t);
+            RightHandTarget.transform.position = Vector3.Lerp(rightStartPos, rightEndPos, t);
+            LeftHandTarget.transform.position = Vector3.Lerp(leftStartPos, leftEndPos, t);
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
 
             yield return new WaitForEndOfFrame();
             time += Time.deltaTime;
         }
 
-        ball.GetComponent<Rigidbody>().mass = 0.001f;
-
-        startPos = RightHandTarget.transform.position;
-        endPos = throwLoc.transform.position;
+        leftStartPos = LeftHandTarget.transform.position;
+        rightStartPos = RightHandTarget.transform.position;
+        
+        var center = GetTargetBallLocation();
+        leftEndPos = LeftHandTarget.transform.position = center - transform.right * radius;
+        rightEndPos = RightHandTarget.transform.position = center + transform.right * radius;
 
         while (time < GrabTime)
         {
-            var t = (time - halfTime) / halfTime;
-            var offset = transform.right * math.sin(t * math.PI);
-            RightHandTarget.transform.position = Vector3.Lerp(startPos, endPos, t) + offset;
+            var t = (time - halfTime) / halfTime; 
+            RightHandTarget.transform.position = Vector3.Lerp(rightStartPos, rightEndPos, t);
+            LeftHandTarget.transform.position = Vector3.Lerp(leftStartPos, leftEndPos, t);
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+            ball.transform.position = Vector3.Lerp(ball.transform.position, GetTargetBallLocation(), t);
 
             yield return new WaitForEndOfFrame();
             time += Time.deltaTime;
@@ -87,27 +115,30 @@ public class Throwing : MonoBehaviour
         grabbing = false;
         lr.SetActive(true);
     }
+    private Vector3 GetTargetBallLocation() => transform.position 
+                                               + transform.forward * (BallOffset.x + GetBallRadius())
+                                               - transform.up * BallOffset.y;
+    private float GetBallRadius() => ball.transform.localScale.x / 2f;
 
     void Throw()
     {
-        grabbedObj.transform.parent.DetachChildren();
-        Rigidbody throwRb = grabbedObj.GetComponent<Rigidbody>();
-        SphereCollider col = grabbedObj.GetComponent<SphereCollider>();
+        Rigidbody throwRb = ball.GetComponent<Rigidbody>();
+        SphereCollider col = ball.GetComponent<SphereCollider>();
         throwRb.constraints = RigidbodyConstraints.None;
-        //grabbedObj.transform.LookAt(forwardObj.transform);
         throwRb.velocity = (transform.up * upThrow + transform.forward * fwdThrow);
-        throwRb.mass = 1.0f;
     }
-    GameObject Nearest()
-    {
-        //temp
-        return throwables[0];
-    }
+    GameObject Nearest() =>
+        balls.Aggregate((min, x) =>
+            Vector3.Distance(transform.position, x.transform.position) <
+            Vector3.Distance(transform.position, min.transform.position)
+                ? x
+                : min);
+
     void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Throwable"))
         {
-            throwables.Add(col.gameObject); 
+            balls.Add(col.gameObject); 
             col.gameObject.GetComponentInChildren<Renderer>().material.SetFloat("_OutlineThickness", 0.0075f);
         }
     }
@@ -116,7 +147,7 @@ public class Throwing : MonoBehaviour
     {
         if (col.CompareTag("Throwable"))
         {
-            throwables.Remove(col.gameObject);
+            balls.Remove(col.gameObject);
             col.gameObject.GetComponentInChildren<Renderer>().material.SetFloat("_OutlineThickness", 0);
         }
     }
