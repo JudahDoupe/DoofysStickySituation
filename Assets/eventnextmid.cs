0696cc33d7c79bd9006c7b8f11699c78c99aceb8
Jacob J. Warren <jjwarren@sep.com>
2022-03-06 15:06:01 -0500

    scenes fixed

---
+++ Assets/eventnextmid.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class eventnextmid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
