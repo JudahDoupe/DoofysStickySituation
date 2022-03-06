b8ccf62a938786dd755ff9f2385a47f394436ca5
Jacob J. Warren <jjwarren@sep.com>
2022-03-06 13:00:33 -0500

    finished ball spawner

--- Assets/BallSpawn.cs
+++ Assets/BallSpawn.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class BallSpawn : MonoBehaviour
 {
     public Transform prefabBall;
-
+    private Transform lastBall;
     // Start is called before the first frame update
     void Start()
     {
         Transform ball = Instantiate(prefabBall, transform.position, Quaternion.identity);
         ball.gameObject.GetComponent<Rigidbody>().AddTorque(Vector3.left * 5);
+        lastBall = ball;
     }
 
     // Update is called once per frame
@@ -20,11 +21,16 @@ public class BallSpawn : MonoBehaviour
         {
             createBall();
         }
+        if (Vector3.Distance(transform.position, lastBall.position) > 15)
+        {
+            createBall();
+        }
     }
 
     public void createBall()
     {
         Transform ball = Instantiate(prefabBall, transform.position, Quaternion.identity);
         ball.gameObject.GetComponent<Rigidbody>().AddTorque(Vector3.left * 5);
+        lastBall = ball;
     }
 }
