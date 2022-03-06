02290b5ed0f9b5fde4ebad7fef98aa2562cc1da4
Jacob J. Warren <jjwarren@sep.com>
2022-03-05 22:26:15 -0500

    fixed launch

--- Assets/Scripts/Launch.cs
+++ Assets/Scripts/Launch.cs
@@ -8,11 +8,15 @@ public class Launch : MonoBehaviour
     private Rigidbody rb;
     public float up;
     public float forward;
+    private DoofyMovement mover;
+    private bool launching;
 
     // Start is called before the first frame update
     void Start()
     {
+        launching = false;
         rb = GetComponent<Rigidbody>();
+        mover = GetComponent<DoofyMovement>();
     }
 
     // Update is called once per frame
@@ -22,10 +26,36 @@ public class Launch : MonoBehaviour
         {
             startLaunch();
         }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            endLaunch();
+        }
+        if (rb.velocity.magnitude == 0 && launching)
+        {
+            endLaunch();
+        }
     }
 
     public void startLaunch()
     {
+        launching = true;
+        rb.constraints = RigidbodyConstraints.None;
         rb.velocity = (Vector3.back * forward + Vector3.up * up);
+        rb.AddTorque(Vector3.forward * 500 + Vector3.left * 500);
+    }
+
+    public void endLaunch()
+    {
+        transform.rotation = Quaternion.identity;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        launching = false;
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.CompareTag("Finish"))
+        {
+            endLaunch();
+        }
     }
 }
